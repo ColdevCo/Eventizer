52a68f7059c4c90030457f806488fa75e7be0274
Naufal Faruqi <naufal.faruqi2010@gmail.com>
2014-08-06 07:10:08 +0700

    Plugin Form

--- assets/js/ev-main.js
+++ assets/js/ev-main.js
@@ -1,13 +1,4 @@
-/**
- * Created by kki on 5/7/14.
- */
 
 jQuery(document).ready(function () {
-	jQuery('.ev-datepicker').datepicker();
-	jQuery('.ev-datetimepicker').datetimepicker();
-	jQuery('.ev-colorpicker').spectrum({
-		flat      : true,
-		showInput : true,
-		allowEmpty: true
-	});
+
 });
--- templates/event-details-form.php
+++ templates/event-details-form.php
@@ -1,112 +1,206 @@
-<style>
-div.details,
-div.location {
-	display: inline-block;
-}
-div.details {
-	margin-right: 8px;
-}
-div.input-group {
-	margin-bottom: 5px;
-}
-div.input-group > label {
-	display: inline-block;
-	width: 180px;
-	vertical-align: top;
-}
-div.input-group > input,
-div.input-group > textarea,
-div.input-group > .google-map {
-	display: inline-block;
-	margin: 0;
-}
-div.input-group > textarea {
-	width: 400px;
-	height: 80px;
-}
-div.input-group > .google-map {
-	width: 400px;
-	height: 200px;
-}
-div.input-group > input[type="text"] {
-	width: 150px;
-}
+<style type="text/css">
+    div.details {
+        margin-right: 8px;
+    }
+
+    .hidden {
+        display: none !important;
+    }
+
+    div.input-group {
+        margin: 0 5px 10px;
+    }
+
+    div.input-group.first {
+        margin: 40px 5px 10px;
+    }
+
+    div.input-group.last {
+        margin: 0px 5px 40px;
+    }
+
+    hr.input-divider {
+        margin: 40px 5px;
+    }
+
+    .datepicker {
+        width: 175px;
+        margin-right: 15px;
+    }
+
+    label.left {
+        display: inline-block;
+        width: 180px;
+        vertical-align: top;
+    }
+
+    label.input-ev_allday {
+        margin-right: 30px;
+    }
+
+    .input-ev_start_time,
+    .input-ev_end_time,
+    .google-map {
+        display: inline-block;
+    }
+
+    #ev_venue_name,
+    #ev_venue_address,
+    .google-map {
+        width: 400px;
+    }
+
+    #ev_venue_address {
+        height: 80px;
+    }
+
+    .google-map {
+        height: 200px;
+    }
 </style>
 
-<div class="input-group">
-	<label for="ev_allday">All day event?</label>
-	<input type="checkbox" name="ev_allday" />
+<div class="input-group first">
+
+    <?= HTML::label('All day event?', 'ev_allday', array('class' => 'label-ev_allday left')); ?>
+
+    <?= HTML::radio('Yes', 'ev_allday', array('class' => 'input-ev_allday')); ?>
+    <?= HTML::radio('No', 'ev_allday', array('class' => 'input-ev_allday', 'checked' => true)); ?>
+
 </div>
 
 <div class="input-group">
-	<label>Start date &amp; time</label>
-	<input type="text" class="datetimepicker" />
+
+    <?= HTML::label('Start date &amp; time', 'ev_start_date', array('class' => 'label-ev_start_date left')); ?>
+
+    <?= HTML::datepicker('ev_start_date', array('class' => 'datepicker', 'id' => 'ev_start_date')); ?>
+    <?= HTML::timepicker('ev_start_time', array('class' => 'timepicker input-ev_start_time')); ?>
+
 </div>
 
 <div class="input-group">
-	<label>End date &amp; time</label>
-	<input type="text" class="datetimepicker" />
+
+    <?= HTML::label('End date &amp; time', 'ev_end_date', array('class' => 'label-ev_end_date left')); ?>
+
+    <?= HTML::datepicker('ev_end_date', array('class' => 'datepicker', 'id' => 'ev_end_date')); ?>
+    <?= HTML::timepicker('ev_end_time', array('class' => 'timepicker input-ev_end_time')); ?>
+</div>
+
+<hr class="input-divider" />
+
+<div class="input-group">
+
+    <?= HTML::label('Venue Name', 'ev_venue_name', array('class' => 'label-ev_venue_name left')); ?>
+    <?= HTML::text('ev_venue_name', array('id' => 'ev_venue_name')); ?>
+
 </div>
 
 <div class="input-group">
-	<label>Location</label>
-	<textarea></textarea>
+
+    <?= HTML::label('Venue Address', 'ev_venue_address', array('class' => 'label-ev_venue_address left')); ?>
+    <?= HTML::textarea('ev_venue_name', array('id' => 'ev_venue_address')); ?>
+
 </div>
 
-<div class="input-group">
-	<label>&nbsp;</label>
-	<div id="map-canvas" class="google-map" data-name='ev_map' data-marker=','></div>
-	<input type='hidden' name='ev_map-lat' value='' />
-	<input type='hidden' name='ev_map-lng' value='' />
+<div class="input-group last">
+
+    <?= HTML::label('Venue Location', 'ev_venue_location', array('class' => 'label-ev_venue_location left')); ?>
+    <?= HTML::geoinput('ev_map', array('class' => 'google-map', 'data-name' => 'ev_map', 'data-marker' => ',')) ?>
+
 </div>
 
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key="></script>
 <script type="text/javascript">
-	var map;
-	var markers = [];
 
-	function initialize() {
-		var mapOptions = {
-			center: new google.maps.LatLng( -34.397, 150.644 ),
-			zoom: 16,
-			scrollwheel: false
-		};
-		map = new google.maps.Map(document.getElementById( "map-canvas" ), mapOptions);
+    jQuery(document).ready(function(){
 
-		if ( jQuery( '#map-canvas' ).attr( 'data-marker' ) !== ',' ) {
-			var markerLat = jQuery( '#map-canvas' ).attr( 'data-marker' ).split(',')[0] || -34.397;
-			var markerLng = jQuery( '#map-canvas' ).attr( 'data-marker' ).split(',')[1] || 150.644;
-			var markerLocation = new google.maps.LatLng( markerLat, markerLng );
+        // Event date script
+        jQuery('.input-ev_allday').bind('change', function(){
+            jQuery('.input-ev_start_time').toggleClass('hidden');
+            jQuery('.input-ev_end_time').toggleClass('hidden');
+        });
 
-			add_marker( markerLocation );
-			map.setCenter( markerLocation );
-		}
+        jQuery('#ev_start_date').datepicker({
+            dateFormat: 'd MM yy',
+            onClose: function( selectedDate ) {
+                jQuery('#ev_end_date').datepicker( "option", "minDate", selectedDate );
+            }
+        });
 
-		google.maps.event.addListener(map, 'click', function(event) {
-			clear_markers();
-			add_marker( event.latLng );
+        jQuery('#ev_end_date').datepicker({
+            dateFormat: 'd MM yy',
+            onClose: function( selectedDate ) {
+                jQuery('#ev_start_date').datepicker( "option", "maxDate", selectedDate );
+            }
+        });
 
-			var name = jQuery( '#map-canvas' ).attr( 'data-name' );
-			jQuery( 'input[name=' + name + '-lat]' ).val( event.latLng.lat() );
-			jQuery( 'input[name=' + name + '-lng]' ).val( event.latLng.lng() );
-		});
-	}
+        jQuery('#ev_venue_address').change(function(){
 
-	function add_marker( location ) {
-		var marker = new google.maps.Marker({
-				position: location,
-				map: map
-		});
-		markers.push( marker );
-	}
+        });
 
-	function clear_markers() {
-		for (var i = 0; i < markers.length; i++) {
-			markers[i].setMap( null );
-		}
+        google.maps.event.addDomListener(window, 'load', include_google_map_script());
+    });
 
-		markers = [];
-	}
+    function include_google_map_script()
+    {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3&sensor=true&callback=init_map';
+        document.body.appendChild(script);
+    }
 
-	google.maps.event.addDomListener(window, 'load', initialize);
+    function init_map()
+    {
+        var map;
+        var marker = [];
+
+        var add_marker = function(location) {
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map
+            });
+            markers.push( marker );
+        }
+
+        var clear_markers = function() {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap( null );
+            }
+
+            markers = [];
+        }
+
+        var mapOptions = {
+            center: new google.maps.LatLng( -34.397, 150.644 ),
+            zoom: 10,
+            scrollwheel: false
+        };
+        map = new google.maps.Map(document.getElementById( "map-ev_map" ), mapOptions);
+
+        if(!!navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                add_marker( geolocate );
+                map.setCenter(geolocate);
+                map.setZoom(16);
+            });
+        }
+
+        if ( jQuery( '#map-ev_map' ).attr( 'data-marker' ) !== ',' ) {
+            var markerLat = jQuery( '#map-ev_map' ).attr( 'data-marker' ).split(',')[0] || -34.397;
+            var markerLng = jQuery( '#map-ev_map' ).attr( 'data-marker' ).split(',')[1] || 150.644;
+            var markerLocation = new google.maps.LatLng( markerLat, markerLng );
+
+            add_marker( markerLocation );
+            map.setCenter( markerLocation );
+            map.setZoom(16);
+        }
+
+        google.maps.event.addListener(map, 'click', function(event) {
+            clear_markers();
+            add_marker( event.latLng );
+
+            var name = jQuery( '#map-canvas' ).attr( 'data-name' );
+            jQuery( 'input[name=' + name + '-lat]' ).val( event.latLng.lat() );
+            jQuery( 'input[name=' + name + '-lng]' ).val( event.latLng.lng() );
+        });
+    }
 </script>
