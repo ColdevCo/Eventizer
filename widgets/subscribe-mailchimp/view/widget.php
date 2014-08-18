e407d8911824c888b1ce77966d15623c0617a773
Naufal Faruqi <naufal.faruqi2010@gmail.com>
2014-08-18 07:38:18 +0700

    subscribe widget style

--- widgets/subscribe-mailchimp/view/widget.php
+++ widgets/subscribe-mailchimp/view/widget.php
@@ -1,11 +1,53 @@
-<div class="panel panel-default">
-	<div class="panel-body">
-		<form role="form" action="?ma=subscribe" method="post">
-			<div class="form-group">
-		        <label for="ev_subscribe-email">Subscribe</label>
-		        <input type="email" class="form-control" name="ev_subscribe-email" id="ev_subscribe-email" placeholder="Email">
-		    </div>
-		    <button type="submit" class="btn btn-default">Submit</button>
-		</form>
-	</div>
+<style type="text/css">
+    #cem-subscribe-mailchimp {
+        background-color: #f3f3f7;
+        padding: 10px;
+        border: 1px solid #ddd;
+        -webkit-border-radius: 5px;
+        -moz-border-radius: 5px;
+        border-radius: 5px;
+    }
+
+    #cem-subscribe-mailchimp .input-group:not(:first-child) {
+        margin-top: 7px;
+    }
+
+    #cem-subscribe-mailchimp .input-group:last-child {
+        margin-top: 20px;
+    }
+
+    #cem-subscribe-mailchimp .input-group > * {
+        display: block;
+        width: 100%;
+    }
+
+    #cem-subscribe-mailchimp .input-group > label {
+        cursor: pointer;
+    }
+
+    #cem-subscribe-mailchimp .input-group > button {
+        width: auto;
+        display: inline-block;
+        padding: 10px 45px;
+    }
+</style>
+<div id="cem-subscribe-mailchimp">
+
+    <form role="form" action="?ma=subscribe" method="post">
+
+        <div class="input-group">
+
+            <?= HTML::label( 'Subscribe', 'cem_subscribe_mailchimp-email' ); ?>
+            <?= HTML::text( 'cem_subscribe_mailchimp-email', array( 'id' => 'cem_subscribe_mailchimp-email', 'placeholder' => 'Email' ) ); ?>
+
+        </div>
+
+        <div class="input-group">
+
+            <?= HTML::button( 'Subscribe' ); ?>
+
+        </div>
+
+    </form>
+
 </div>
