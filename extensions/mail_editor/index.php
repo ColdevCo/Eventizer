6154ba010bcb4059f4ff3b706c22e2a354981055
Naufal Faruqi <naufal.faruqi2010@gmail.com>
2014-08-25 07:50:45 +0700

    mail editor

--- extensions/mail_editor/index.php
+++ extensions/mail_editor/index.php
@@ -40,6 +40,53 @@ class EventMail {
             return $tabs;
         });
     }
+
+    public static function get_mails() {
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . "event_mails";
+
+        $tickets = $wpdb->get_results( "SELECT * FROM `{$table_name}`" );
+
+        return $tickets;
+    }
+
+    public static function get_mail( $mail_id = 0 ) {
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . "event_mails";
+
+        $tickets = $wpdb->get_results( "SELECT * FROM `{$table_name}` WHERE `id` >= {$mail_id} ORDER BY `id` ASC LIMIT 1" );
+
+        return $tickets[0];
+    }
+
+    public static function get_mail_by_context( $context ) {
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . "event_mails";
+
+        $tickets = $wpdb->get_results( "SELECT * FROM `{$table_name}` WHERE `context` = '{$context}' ORDER BY `id` ASC LIMIT 1" );
+
+        return $tickets[0];
+    }
+
+    public static function update( $mail_id, $data ) {
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . "event_mails";
+
+        $wpdb->update(
+            $table_name,
+            array(
+                'subject' => $data['cem_mail_editor_subject'],
+                'content' => $data['cem_mail_editor_content']
+            ),
+            array( 'id' => $mail_id ),
+            array( '%s', '%s' ),
+            array( '%d' )
+        );
+    }
 }
 
 return new EventMail();
--- extensions/mail_editor/view/setting-mail.php
+++ extensions/mail_editor/view/setting-mail.php
@@ -1,25 +1,73 @@
+<?php
+
+if ( $_POST ) {
+    EventMail::update( $_GET['mail_id'], $_POST );
+}
+?>
+<style type="text/css">
+
+    #cem-mail-editor .input-group {
+        margin-bottom: 20px;
+    }
+
+    #cem-mail-editor .input-group > * {
+        display: block;
+    }
+
+    #cem_mail_editor_context,
+    #cem_mail_editor_subject {
+        padding: 5px 2px;
+        width: 100%;
+    }
+
+    #cem-mail-editor .input-group > button {
+        display: inline-block;
+        cursor: pointer;
+        background-color: #16499A;
+        color: #FFF;
+        padding: 6px 30px;
+        margin: 25px 0 0;
+        text-align: center;
+        vertical-align: middle;
+        border: 1px solid transparent;
+        -webkit-border-radius: 0;
+        -moz-border-radius: 0;
+        border-radius: 0;
+    }
+
+</style>
+
 <div id="cem-mail-editor">
 
-    <?php $form = new Form(); $form->open( '', array( 'method' => 'post' ) ); ?>
+    <?php $form = new Form(); echo $form->open( '', array( 'method' => 'post' ) ); ?>
+
+    <?php
+    $mail = EventMail::get_mail( isset($_GET['mail_id']) ? $_GET['mail_id'] : 0 );
+    if ( $mail ) :
+    ?>
 
     <div class="input-group">
 
-        <?= HTML::label( 'Context', 'cem_mail_editor_context' ); ?>
-        <?= HTML::dropdown( 'cem_mail_editor_context', array( 'test' => 'test' ) ); ?>
+        <?php
+        $mails = EventMail::get_mails();
+        $mails = array_reduce($mails, function($result, $data){ $result["{$data->id} "] = $data->context; return $result; }, array());
+
+        echo HTML::label( 'Context', 'cem_mail_editor_context' );
+        echo HTML::dropdown( 'cem_mail_editor_context', $mails, array( 'id' => 'cem_mail_editor_context', 'value' => "{$mail->id} " ) );
+        ?>
 
     </div>
 
     <div class="input-group">
 
         <?= HTML::label( 'Subject', 'cem_mail_editor_subject' ); ?>
-        <?= HTML::text( 'cem_mail_editor_subject' ); ?>
+        <?= HTML::text( 'cem_mail_editor_subject', array( 'id' => 'cem_mail_editor_subject', 'value' => $mail->subject ) ); ?>
 
     </div>
 
     <div class="input-group">
 
-        <?= HTML::label( 'Content', 'cem_mail_editor_content' ); ?>
-        <?= HTML::textarea( 'cem_mail_editor_content', array( 'class' => 'wp-core-ui wp-editor-wrap tmce-active has-dfw' ) ); ?>
+        <?php wp_editor( $mail->content, 'cem_mail_editor_content' ); ?>
 
     </div>
 
@@ -29,6 +77,15 @@
 
     </div>
 
-    <?php $form->close(); ?>
+    <?php endif; echo $form->close(); ?>
 
 </div>
+
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        jQuery('#cem_mail_editor_context').on('change', function(){
+            var id = jQuery(this).val();
+            window.location.replace('<?= add_query_arg( array('mail_id' => false) ) ?>&mail_id=' + id);
+        });
+    });
+</script>
